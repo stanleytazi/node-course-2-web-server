56f963793311676b2d3314b36d7e6048f33be67d
stanleytazi <stanle52042@msn.com>
2018-01-15 17:49:44 +0800

    Add project page

--- server.js
+++ server.js
@@ -40,6 +40,12 @@ app.get('/about', (req, res) => {
 });
 
 
+app.get('/project', (req, res) => {
+    res.render('project.hbs', {
+       pageTitle: 'Project Page' 
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         msg: 'goodjob',
