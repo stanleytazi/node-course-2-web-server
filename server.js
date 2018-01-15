7c22fcd12186e9a84af4538d6e2f461eded21403
stanleytazi <stanle52042@msn.com>
2018-01-15 16:37:11 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const fs = require('fs');
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log+'\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+hbs.registerHelper('screemIt', (text) => {
+    return text.toUpperCase();
+});
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcomem to my website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+
+app.get('/bad', (req, res) => {
+    res.send({
+        msg: 'goodjob',
+        name: 'chun',
+        favorite: [
+            'cake',
+            'coffee'
+        ]
+});
+});
+
+
+app.listen(3000);
